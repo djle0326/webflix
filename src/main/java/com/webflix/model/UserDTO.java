1b6b18104ea5c5c9036655d256346d9cd2a4d9a9
fkdltm2023621025 <2023621025@m.jeiu.ac.kr>
2024-06-21 03:53:31 +0900

    Update UserDTO.java

--- src/main/java/com/webflix/model/UserDTO.java
+++ src/main/java/com/webflix/model/UserDTO.java
@@ -1,66 +1,122 @@
 package com.webflix.model;
 
-public class UserDTO {
+import java.sql.Timestamp;
 
-    public enum Gender {
-        M, F, O  
-    }
-    
+public class UserDTO {
+    private String userId;
     private String name;
     private String birthDate;
-    private Gender gender;  
+    private Gender gender;
     private String nickname;
-    private String userId;
     private String password;
-    private String phone;
+    private String phoneNumber;
     private String email;
+    private String boardId;
+    private String title;
+    private String content;
+    private Timestamp createdAt;
+
+    // Enum 타입 정의
+    public enum Gender {
+        M, F, O
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getBirthDate() {
         return birthDate;
     }
+
     public void setBirthDate(String birthDate) {
         this.birthDate = birthDate;
     }
+
     public Gender getGender() {
         return gender;
     }
+
     public void setGender(Gender gender) {
         this.gender = gender;
     }
+
     public String getNickname() {
         return nickname;
     }
+
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
-    public String getUserId() {
-        return userId;
-    }
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
-    public String getPhone() {
-        return phone;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
-    public void setPhone(String phone) {
-        this.phone = phone;
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+    public String getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(String boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    
 }
+
+
